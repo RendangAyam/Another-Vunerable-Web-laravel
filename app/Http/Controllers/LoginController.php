effca9a257aa3711f9a4e0dc698b4d69d0a4a743
Andrew Christian Widjojo <90097535+RendangAyam@users.noreply.github.com>
2023-01-10 23:41:03 +0700

    bug fix login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Models\User;
+use App\Models\Login;
+use Carbon\Carbon;
 use DB;
 use Auth;
 use Session;
@@ -22,9 +24,18 @@ class LoginController extends Controller
     public function doLogin( Request $request )
     {
         $user = DB::select('SELECT `active` FROM users WHERE email = \''.$request->email.'\'');
-        if($user[0]->active == 0){
-            Session::flash('message', 'wrong username or password or account didn\'t active');
-            return back();
+        // $login = DB::select('SELECT * FROM logins WHERE email = \''.$request->email.'\'');
+        // if($login){
+        //     if(!isset($login[0]->logout_at)){
+        //         Session::flash('message', 'session already exist');
+        //         return redirect('login');
+        //     }
+        // }
+        if($user){
+            if($user[0]->active == 0){
+                Session::flash('message', 'wrong username or password or account didn\'t active');
+                return back();
+            }
         }
         $request->validate([
             'email'=>'required|email',
@@ -33,6 +44,21 @@ class LoginController extends Controller
         $credentials = $request->only(['email', 'password', 'active' => 1, 'role'=>'User']);
         // dd($credentials);
         if (Auth::attempt($credentials)) {
+            // dd($login);
+            // if(!$login){
+            //     $tkn = Login::create([
+            //         'email' => $request->email,
+            //         'login_at' => Carbon::now(),
+            //         'logout_at' => NULL,
+            //     ]);
+            // }
+            // else{
+            //     $tkn = Login::where('email', $request->email)->
+            //     update([
+            //         'login_at' => Carbon::now(),
+            //         'logout_at' => NULL,
+            //     ]);
+            // }
             $request->session()->regenerate();
             return redirect()->intended('home');
         }
@@ -43,6 +69,11 @@ class LoginController extends Controller
 
     public function logout(Request $request)
     {
+        $tkn = Login::where('email', Auth::User()->email)->
+                update([
+                    'logout_at' => Carbon::now(),
+        ]);
+
         Auth::logout();
     
         $request->session()->invalidate();
