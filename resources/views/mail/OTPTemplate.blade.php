113e4c51f1a118e471bfc361b320fb5395f41e3b
Andrew Christian Widjojo <90097535+RendangAyam@users.noreply.github.com>
2023-01-10 23:20:43 +0700

    update /

--- resources/views/mail/OTPTemplate.blade.php
+++ resources/views/mail/OTPTemplate.blade.php
@@ -6,7 +6,7 @@
   <h5>link only valid in 3 minutes</h5>
 </center>
 
-<h4>If you didn\'t request this forget password link, please go to your Profile page and change your password right away.For assistance, please contact AVW Help.
-Thanks for helping us maintain your account\'s security.</h4>
+<h4>If you didn't request this forget password link, please go to your Profile page and change your password right away.For assistance, please contact AVW Help.
+Thanks for helping us maintain your account's security.</h4>
 <p>Happy Hunting,</p>
 <p>The AVW Team</p>
