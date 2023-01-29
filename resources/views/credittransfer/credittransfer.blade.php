7c577b52808daf3c776263c5be213b0e2df2c98b
Andrew Christian Widjojo <90097535+RendangAyam@users.noreply.github.com>
2023-01-29 16:19:57 +0700

    Changes

--- resources/views/credittransfer/credittransfer.blade.php
+++ resources/views/credittransfer/credittransfer.blade.php
@@ -8,7 +8,7 @@
             Register Card number
         </a>
         <a href="{{ route('credittransfer/listcard') }}" class="my-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            List Card number
+            Transfer
         </a>
         <div>
             @if(session('message'))
