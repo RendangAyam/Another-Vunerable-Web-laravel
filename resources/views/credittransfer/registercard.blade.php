6ed6147476af11cae13dffcfe5214cd6b65ddf59
Andrew Christian Widjojo <90097535+RendangAyam@users.noreply.github.com>
2023-02-02 00:39:31 +0700

    fix button

--- resources/views/credittransfer/registercard.blade.php
+++ resources/views/credittransfer/registercard.blade.php
@@ -13,7 +13,7 @@
             <input type="hidden" name="destcard" id="destcard" value="{{ $card[0]->cardnumber }}">
             <input type="hidden" name="srccard" id="srccard" value="{{ Auth::User()->cardnumber }}">
             <button type="submit" name="register" class="flex justify-center my-2 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Search
+                Register
             </button>
         </form>
     </div>
